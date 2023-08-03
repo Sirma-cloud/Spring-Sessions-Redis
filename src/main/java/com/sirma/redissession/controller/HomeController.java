6195525b755d7b88e4e23848253ca679b25846c6
Sirma-cloud <kevinkiplimo99@gmail.com>
2023-08-03 12:33:55 +0300

    This is a sessions project to manage user sessions in your java applications

---
+++ src/main/java/com/sirma/redissession/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.sirma.redissession.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class HomeController {
+    private final String HOME_VIEW_COUNT = "HOME_VIEW_COUNT";
+
+    @GetMapping("/")
+    public String home(Principal principal, HttpSession session) {
+        incrementCount(session, HOME_VIEW_COUNT);
+        return "Hello, " + principal.getName();
+    }
+
+    @GetMapping("/count")
+    public String count(HttpSession session) {
+        return "HOME_VIEW_COUNT: " + session.getAttribute(HOME_VIEW_COUNT);
+    }
+
+    private void incrementCount(HttpSession session, String attr) {
+        Integer homeViewCount = (Integer) session.getAttribute(attr);
+        if (homeViewCount == null) {
+            homeViewCount = 0;
+        }
+        homeViewCount++;
+        session.setAttribute(attr, homeViewCount);
+    }
+}
---
+++ src/test/java/com/sirma/redissession/RedisSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirma.redissession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
